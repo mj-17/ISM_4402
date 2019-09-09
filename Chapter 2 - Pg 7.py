a396de8ef37ec385a7eefcbb98d8dc4282109eba
mj-17 <54560133+mj-17@users.noreply.github.com>
2019-09-08 21:04:59 -0400

    Add files via upload

---
+++ Chapter 2 - Pg 7.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd 
+Location = "Dropbox/USF/Fall 2019/ISM 4402/CensusData.csv"
+df = pd.read_csv(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
