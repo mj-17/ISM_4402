b8f43aab9b2a6daa7ba0712f66b4d71da4b3a9c7
mj-17 <54560133+mj-17@users.noreply.github.com>
2019-09-15 21:49:50 -0400

    Add files via upload

---
+++ Chapter 2 - Pg 14.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Connect to sqlite db
+db_file = r'datasets/salesdata.db'
+engine = create_engine(r"sqlite:///{}".format(db_file))
+
+sql = "SELECT * FROM scores;"
+
+scores_df = pd.read_sql(sql, engine)
+
+scores_df.head()
+
+
+
+# In[ ]:
+
+
+
+
